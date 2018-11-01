f15fe4fc47872c46b58dc1cb3efb64a7a42885c8
Jimmy Riguelle <jimmyriguelle@hotmail.com>
2018-11-01 01:11:12 +0100

    add some new pages

---
+++ App2/App2/model/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.model
+{
+    class User
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+
+
+
+    }
+}
